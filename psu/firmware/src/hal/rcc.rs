c8afb65879f318f9b38613a42e76107c0f6af4cf
Adam Greig <adam@adamgreig.com>
2019-10-23 01:47:46 +0100

    WIP firmware

---
+++ psu/firmware/src/hal/rcc.rs
@@ -0,0 +1,54 @@
+use stm32ral::{rcc, flash, read_reg, modify_reg};
+
+pub struct RCC {
+    rcc: rcc::Instance,
+    flash: flash::Instance,
+}
+
+impl RCC {
+    pub fn new(rcc: rcc::Instance, flash: flash::Instance) -> Self {
+        RCC { rcc, flash }
+    }
+
+    /// Set up device clocks
+    pub fn setup(&self) {
+        let flash = &self.flash;
+        let rcc = &self.rcc;
+
+        // Configure flash to two wait states ready for 70MHz operation
+        modify_reg!(stm32ral::flash, flash, ACR, LATENCY: 2);
+
+        // Ensure HSI is enabled, stable, and in use
+        modify_reg!(stm32ral::rcc, rcc, CR, HSION: On);
+        while read_reg!(stm32ral::rcc, rcc, CR, HSIRDY != Ready) {}
+        modify_reg!(stm32ral::rcc, rcc, CFGR, SW: HSI);
+        while read_reg!(stm32ral::rcc, rcc, CFGR, SWS != HSI) {}
+
+        // Enable HSE
+        modify_reg!(stm32ral::rcc, rcc, CR, HSEBYP: Bypassed);
+        modify_reg!(stm32ral::rcc, rcc, CR, HSEON: On);
+        while read_reg!(stm32ral::rcc, rcc, CR, HSERDY != Ready) {}
+
+        // Ensure PLL is off before configuring
+        modify_reg!(stm32ral::rcc, rcc, CR, PLLON: Off);
+        while read_reg!(stm32ral::rcc, rcc, CR, PLLRDY == Ready) {}
+
+        // Configure PLL: 25MHz HSE /5 *14 = 70MHz PLL
+        // APB1: 35MHz, APB2: 70MHz, AHB: 70MHz
+        modify_reg!(stm32ral::rcc, rcc, CFGR2, PREDIV: Div5);
+        modify_reg!(stm32ral::rcc, rcc, CFGR, PLLSRC: HSE_Div_PREDIV, PLLMUL: Mul14);
+        modify_reg!(stm32ral::rcc, rcc, CFGR, PPRE2: Div1, PPRE1: Div2, HPRE: Div1);
+        modify_reg!(stm32ral::rcc, rcc, CR, PLLON: On);
+
+        // Wait for PLL to be ready and swap to it
+        while read_reg!(stm32ral::rcc, rcc, CR, PLLRDY != Ready) {}
+        modify_reg!(stm32ral::rcc, rcc, CFGR, SW: PLL);
+        while read_reg!(stm32ral::rcc, rcc, CFGR, SWS != PLL) {}
+
+        // Enable peripheral clocks
+        modify_reg!(stm32ral::rcc, rcc, AHBENR,
+                    ADC12EN: Enabled, IOPAEN: Enabled, IOPBEN: Enabled, DMA1EN: Enabled);
+        modify_reg!(stm32ral::rcc, rcc, APB1ENR, DAC1EN: Enabled, DAC2EN: Enabled);
+        modify_reg!(stm32ral::rcc, rcc, APB2ENR, HRTIM1EN: Enabled, USART1EN: Enabled);
+    }
+}
--- psu/firmware/src/main.rs
+++ psu/firmware/src/main.rs
@@ -2,10 +2,11 @@
 #![no_main]
 
 use panic_halt as _;
-use cortex_m_semihosting::hprintln;
 use rtfm::cyccnt::U32Ext;
 
-use stm32ral::{read_reg, write_reg, modify_reg};
+use stm32ral::{write_reg, modify_reg};
+
+pub mod hal;
 
 #[rtfm::app(device=stm32ral::stm32f3::stm32f3x4, monotonic=rtfm::cyccnt::CYCCNT, peripherals=true)]
 const APP: () = {
@@ -13,38 +14,54 @@ const APP: () = {
         gpiob: stm32ral::gpio::Instance,
     }
 
-    #[init(spawn=[foo])]
+    #[init(spawn=[heartbeat])]
     fn init(mut cx: init::Context) -> init::LateResources {
-        hprintln!("init").unwrap();
-
         cx.core.DCB.enable_trace();
         cx.core.DWT.enable_cycle_counter();
 
-        let rcc = &cx.device.RCC;
+        // Initialise device clocks
+        let rcc = hal::rcc::RCC::new(cx.device.RCC, cx.device.Flash);
+        rcc.setup();
+
+        // Initialise GPIOs
         let gpioa = &cx.device.GPIOA;
         let gpiob = &cx.device.GPIOB;
-
-        modify_reg!(stm32ral::rcc, rcc, AHBENR, IOPAEN: Enabled, IOPBEN: Enabled);
         modify_reg!(stm32ral::gpio, gpioa, MODER, MODER8: Output);
         modify_reg!(stm32ral::gpio, gpioa, ODR, ODR8: 0);
         modify_reg!(stm32ral::gpio, gpiob, MODER, MODER3: Output, MODER4: Output);
         modify_reg!(stm32ral::gpio, gpiob, ODR, ODR3: 0, ODR4: 0);
 
-        cx.spawn.foo().unwrap();
+        // Start heartbeat task
+        cx.spawn.heartbeat().unwrap();
 
         init::LateResources { gpiob: cx.device.GPIOB }
     }
 
-    #[task(resources=[gpiob], schedule=[foo])]
-    fn foo(cx: foo::Context) {
-        if read_reg!(stm32ral::gpio, cx.resources.gpiob, ODR, ODR3 == 0) {
+    // Heartbeat task runs 10 times a second and toggles an LED
+    #[task(resources=[gpiob], schedule=[heartbeat])]
+    fn heartbeat(cx: heartbeat::Context) {
+        static mut STATE: bool = false;
+        *STATE = !*STATE;
+        if *STATE {
             write_reg!(stm32ral::gpio, cx.resources.gpiob, BSRR, BS3: Set);
         } else {
             write_reg!(stm32ral::gpio, cx.resources.gpiob, BSRR, BR3: Reset);
         }
-        cx.schedule.foo(cx.scheduled + 1_000_000.cycles()).unwrap();
+        cx.schedule.heartbeat(cx.scheduled + 7_000_000.cycles()).unwrap();
     }
 
+    // Manually define an idle task with just NOPs to prevent the microcontroller
+    // entering sleep mode, which makes attaching the debugger more annoying but
+    // has no benefit in this mains-powered application.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
+        }
+    }
+
+    // We require at least one interrupt vector defined here per software task
+    // priority level in use, for RTFM to co-opt for software task execution.
     extern "C" {
         fn FLASH();
     }
